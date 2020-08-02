13d413b3c6fbb1e19b424c23f366206e0c6c0ed4
jmingrove21 <jmingrove21@gmail.com>
2020-08-02 23:28:42 +0900

    장바구니 format 설정. line item 숫자 증감 진행 예정

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,4 @@
 class LineItem < ApplicationRecord
     belongs_to :order, optional: true
-
+    belongs_to :item, optional: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
     belongs_to :user, optional:true
     has_many :line_items, dependent: :nullify
 
+
+    has_many :buy_item, through: :line_items
 end
